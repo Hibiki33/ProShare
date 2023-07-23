4a53da7ff04574d3e62ed84584ebc11f92bb16a2
BUAA-WFY <42094053+jayfeather9@users.noreply.github.com>
2023-07-23 14:42:32 +0800

    simple update

--- main/views.py
+++ main/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def main_view(request):
-    return render(request, 'test11.html')
+    return render(request, 'mainview.html')
     # return render(request, 'main.html')
 

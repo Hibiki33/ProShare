4a789effdb4b639f39bb00a59afb324f83de146b
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-07-24 23:14:23 +0800

    account: debug gitignore

--- ProShare/settings.py
+++ ProShare/settings.py
@@ -136,3 +136,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'

6a13ddbec1918f69b83291e332febce4ba4f3766
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-07-24 12:12:03 +0800

    question: model, lib

--- question/models.py
+++ question/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=32)
+    content = models.TextField()
+
+    # the following is used to confirm whether there is a standard answer
+    std_ans = models.BooleanField(default=False)
+    answer = models.TextField(blank=True)
+
+    # the following indicates the create and update time
+    # in case of using `.objects.filter().update()` method,
+    #   update time won't change automatically,
+    #   thus we need to use `auto_now` to update the update time.
+    # if we use `.save()` method,
+    #   update time will change automatically,
+    #   but I don't know if `save()` will create a new question or not.
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey('account.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.question_id) + ' ' + self.title
--- question/views.py
+++ question/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def main_view(request):
-    return render(request, 'questions.html')
+def question_lib_page(request):
+    if request.method == 'GET':
+        return render(request, 'question_lib.html')
+    elif request.method == 'POST':
+        pass

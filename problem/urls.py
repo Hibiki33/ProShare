521335aae58cf1f94fabf40a3e50386559c37828
jayfeather9 <jayfeather9@qq.com>
2023-07-24 15:19:53 +0800

    problem

--- problem/urls.py
+++ problem/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.problem_lib_page),
+    path("<int:question_id>/", views.detail),
 ]
--- problem/views.py
+++ problem/views.py
@@ -7,3 +7,9 @@ def problem_lib_page(request):
         return render(request, 'problem_group_list.html')
     elif request.method == 'POST':
         pass
+
+def detail(request, question_id):
+    if request.method == 'GET':
+        return render(request, 'problem_detail.html')
+    elif request.method == 'POST':
+        pass

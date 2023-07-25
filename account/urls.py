18a3492239a0f8e44677a5270b5528d83ad8d54a
钟彦童 <21373209@buaa.edu.cn>
2023-07-25 23:31:41 +0800

    logout

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('change_password/', views.change_password_page),
     path('edit/', views.edit_page),
     path('login/', views.login_page),
+    path('logout/', views.logout_page),
     path('register/', views.register_page)
 ]

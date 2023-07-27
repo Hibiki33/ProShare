4085afba2d1c11bed1e664229c7424ece12f5f99
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-07-27 17:43:35 +0800

    guardian settings

--- ProShare/settings.py
+++ ProShare/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'guardian',
     'main',
     'account',
     'problem',
@@ -140,3 +141,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'guardian.backends.ObjectPermissionBackend',
+]
+
+ANONYMOUS_USER_ID = - 1

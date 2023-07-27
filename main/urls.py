ae63d333fbc9148f32cd74de5231115e20ed995c
Red <ProjektRed@buaa.edu.cn>
2023-07-27 17:26:01 +0800

    upd: 404

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.main_view),
     path('test/', views.test_view),
+    path('404', views.not_found_view),
 ]
--- main/views.py
+++ main/views.py
@@ -7,3 +7,6 @@ def main_view(request):
 
 def test_view(request):
     return render(request, 'group_list.html')
+
+def not_found_view(request):
+    return render(request, '404.html')

307573277aaf4311568650cf3ec4462eebcc54cb
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-07-27 15:35:27 +0800

    admin: user manage

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import User
 
-admin.site.register(User)
 
+class UserAdmin(admin.ModelAdmin):
+    # list view
+    list_display = ('username', 'email', 'is_superuser', 'register_date')
+
+    # 10 items per page
+    list_per_page = 10
+
+    # search box
+    search_fields = ('username',)
+
+    # filter options
+    list_filter = ('is_superuser',)
+
+    # display register date in green
+    def register_date(self, obj):
+        return format_html(
+            '<span style="color: green;">{}</span>',
+            obj.date_joined.strftime('%Y-%m-%d %H:%M:%S')
+        )
+
+
+admin.site.register(User, UserAdmin)

40cc9ff463138164e69120843ffa9a20ab9ba9c7
BUAA-WFY <42094053+jayfeather9@users.noreply.github.com>
2023-07-28 18:29:09 +0800

    add: set findadd

--- main/views.py
+++ main/views.py
@@ -8,7 +8,15 @@ def main_view(request):
 
 
 def test_view(request):
-    return render(request, 'problem_set_findadd.html')
+    a = [
+        {"ID": 1, "Name": "Question A", "Diff": "Easy", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 2, "Name": "Question B", "Diff": "Medium", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 3, "Name": "Question C", "Diff": "Hard", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 4, "Name": "Question D", "Diff": "Easy", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 5, "Name": "Question E", "Diff": "Medium", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 6, "Name": "Question F", "Diff": "Hard", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"}
+    ]
+    return render(request, 'problem_set_findadd.html', {"problem_info_list": a})
 
 
 def not_found_view(request):

4958df851ff25afa0bc2622574c40c017cb4e881
BUAA-WFY <42094053+jayfeather9@users.noreply.github.com>
2023-07-28 19:08:01 +0800

    add: problem set create

--- main/views.py
+++ main/views.py
@@ -8,22 +8,23 @@ def main_view(request):
 
 
 def test_view(request):
-    # a = [
-    #     {"ID": 1, "Name": "Question A", "Diff": "Easy", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
-    #     {"ID": 2, "Name": "Question B", "Diff": "Medium", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
-    #     {"ID": 3, "Name": "Question C", "Diff": "Hard", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
-    #     {"ID": 4, "Name": "Question D", "Diff": "Easy", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
-    #     {"ID": 5, "Name": "Question E", "Diff": "Medium", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
-    #     {"ID": 6, "Name": "Question F", "Diff": "Hard", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"}
-    # ]
-    # return render(request, 'problem_set_findadd.html', {"problem_info_list": a})
     a = [
-        {"id": 1, "name": "Question A", "created_by": "Easy", "belongs_to": "Tag1"},
-        {"id": 2, "name": "Question B", "created_by": "Medium", "belongs_to": "Tag1"},
-        {"id": 3, "name": "Question C", "created_by": "Hard", "belongs_to": "Tag1"},
-        {"id": 4, "name": "Question D", "created_by": "Easy", "belongs_to": "Tag1"},
+        {"ID": 1, "Name": "Question A", "Diff": "Easy", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 2, "Name": "Question B", "Diff": "Medium", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 3, "Name": "Question C", "Diff": "Hard", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 4, "Name": "Question D", "Diff": "Easy", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 5, "Name": "Question E", "Diff": "Medium", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"},
+        {"ID": 6, "Name": "Question F", "Diff": "Hard", "Tag1": "Tag1", "Tag2": "Tag2", "Tag3": "Tag3"}
     ]
-    return render(request, 'problem_set_list.html', {"problem_set_list": a})
+    # return render(request, 'problem_set_findadd.html', {"problem_info_list": a})
+    # a = [
+    #     {"id": 1, "name": "Question A", "created_by": "Easy", "belongs_to": "Tag1"},
+    #     {"id": 2, "name": "Question B", "created_by": "Medium", "belongs_to": "Tag1"},
+    #     {"id": 3, "name": "Question C", "created_by": "Hard", "belongs_to": "Tag1"},
+    #     {"id": 4, "name": "Question D", "created_by": "Easy", "belongs_to": "Tag1"},
+    # ]
+    # return render(request, 'problem_set_list.html', {"problem_set_list": a})
+    return render(request, 'problem_set_create.html', {"problem_info_list": a, "name": "Test Set", "isPublic": False})
 
 
 def not_found_view(request):
